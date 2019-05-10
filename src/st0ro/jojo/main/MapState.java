90cc50d0d288169d9c7133791d009124243eeebd
st0ro <46041969+st0ro@users.noreply.github.com>
2019-05-10 01:01:40 -0600

    Update MapState.java
    not broken anymore

--- src/st0ro/jojo/main/MapState.java
+++ src/st0ro/jojo/main/MapState.java
@@ -37,14 +37,14 @@ public class MapState extends BasicGameState{
 		{
 			for(int i = 1; i <= delta; i++) //run once for every millisecond passed since last update, ensures smoothness across devices and system load
 			{
-				if(map.getTile((Math.round(x), (int)Math.round(y+0.5)).getType() != 0)
+				if(map.getTile(Math.round(x), (int)Math.round(y+0.5)).getType() != 0)
 				{
 					//loadMap(map);
 				}
 				else if(!map.getTile((int) Math.floor(x+0.1), (int) Math.round(y+0.5)).getBlock() && !map.getTile((int) Math.ceil(x-0.1), (int) Math.round(y+0.5)).getBlock())
 				{
 					y+=0.005; //if can move, move
-					if(running && !map.getTile((int) Math.floor(x+0.1), (int) Math.round(y+0.5)) && !map.getTile((int) Math.ceil(x-0.1), (int) Math.round(y+0.5)))
+					if(running && !map.getTile((int) Math.floor(x+0.1), (int) Math.round(y+0.5)).getBlock() && !map.getTile((int) Math.ceil(x-0.1), (int) Math.round(y+0.5)).getBlock())
 					{
 						y+=0.005; //if can move more and running, move more and set running animation
 						maySprite.speed = 2;
@@ -58,14 +58,14 @@ public class MapState extends BasicGameState{
 		{
 			for(int i = 1; i <= delta; i++)
 			{
-				if(map.getType(Math.round(x), (int)Math.round(y-0.5)) != 0)
+				if(map.getTile(Math.round(x), (int)Math.round(y-0.5)).getType() != 0)
 				{
 					
 				}
-				else if(!map.getTile((int) Math.floor(x+0.1), (int) Math.round(y-0.5)) && !map.getTile((int) Math.ceil(x-0.1), (int) Math.round(y-0.5)))
+				else if(!map.getTile((int) Math.floor(x+0.1), (int) Math.round(y-0.5)).getBlock() && !map.getTile((int) Math.ceil(x-0.1), (int) Math.round(y-0.5)).getBlock())
 				{
 					y-=0.005;
-					if(running && !map.getTile((int) Math.floor(x+0.1), (int) Math.round(y-0.5)) && !map.getTile((int) Math.ceil(x-0.1), (int) Math.round(y-0.5)))
+					if(running && !map.getTile((int) Math.floor(x+0.1), (int) Math.round(y-0.5)).getBlock() && !map.getTile((int) Math.ceil(x-0.1), (int) Math.round(y-0.5)).getBlock())
 					{
 						y-=0.005;
 						maySprite.speed = 2;
@@ -80,14 +80,14 @@ public class MapState extends BasicGameState{
 		{
 			for(int i = 1; i <= delta; i++)
 			{
-				if(map.getType((int) Math.round(x-0.5), Math.round(y)) != 0)
+				if(map.getTile((int) Math.round(x-0.5), Math.round(y)).getType() != 0)
 				{
 					
 				}
-				else if(!map.getTile((int) Math.round(x-0.5), (int) Math.floor(y+0.1)) && !map.getTile((int) Math.round(x-0.5), (int) Math.ceil(y-0.1)))
+				else if(!map.getTile((int) Math.round(x-0.5), (int) Math.floor(y+0.1)).getBlock() && !map.getTile((int) Math.round(x-0.5), (int) Math.ceil(y-0.1)).getBlock())
 				{
 					x-=0.005;
-					if(running && !map.getTile((int) Math.round(x-0.5), (int) Math.floor(y+0.1)) && !map.getTile((int) Math.round(x-0.5), (int) Math.ceil(y-0.1)))
+					if(running && !map.getTile((int) Math.round(x-0.5), (int) Math.floor(y+0.1)).getBlock() && !map.getTile((int) Math.round(x-0.5), (int) Math.ceil(y-0.1)).getBlock())
 					{
 						x-=0.005;
 						maySprite.speed = 2;
@@ -102,14 +102,14 @@ public class MapState extends BasicGameState{
 		{
 			for(int i = 1; i <= delta; i++)
 			{
-				if(map.getType((int) Math.round(x-0.5), Math.round(y)) != 0)
+				if(map.getTile((int) Math.round(x-0.5), Math.round(y)).getType() != 0)
 				{
 					
 				}
-				else if(!map.getTile((int) Math.round(x+0.5), (int) Math.floor(y+0.1)) && !map.getTile((int) Math.round(x+0.5), (int) Math.ceil(y-0.1)))
+				else if(!map.getTile((int) Math.round(x+0.5), (int) Math.floor(y+0.1)).getBlock() && !map.getTile((int) Math.round(x+0.5), (int) Math.ceil(y-0.1)).getBlock())
 				{
 					x+=0.005;
-					if(running && !map.getTile((int) Math.round(x+0.5), (int) Math.floor(y+0.1)) && !map.getTile((int) Math.round(x+0.5), (int) Math.ceil(y-0.1)))
+					if(running && !map.getTile((int) Math.round(x+0.5), (int) Math.floor(y+0.1)).getBlock() && !map.getTile((int) Math.round(x+0.5), (int) Math.ceil(y-0.1)).getBlock())
 					{
 						x+=0.005;
 						maySprite.speed = 2;
@@ -127,7 +127,7 @@ public class MapState extends BasicGameState{
 	public int getID() {
 		return 0; //used to change game states for StateBasedGame
 	}
-	private void loadMap(String map, int x, int y)
+	//private void loadMap(String map, int x, int y)
 	{
 		
 	}
